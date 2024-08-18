07de5ea506191f8d64db42c6a8a9f409b4dcddff
Polina Kyzylova <kyzylovapa@mail.ru>
2024-08-18 15:49:07 +0500

    fix: min-max container height + scroll

--- src/components/templates/AdminBudgetLayout/AdminBudgetLayout.jsx
+++ src/components/templates/AdminBudgetLayout/AdminBudgetLayout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
+
 export default function AdminBudgetLayout() {
   return (
     <div>AdminBudgetLayout</div>
--- src/components/templates/AdminCFOLayout/AdminCFOLayout.jsx
+++ src/components/templates/AdminCFOLayout/AdminCFOLayout.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styles from './AdminCFOLayout.module.css';
+
 
 export default function AdminCFOLayout() {
   return (
-    <div>AdminCFOLayout</div>
+    <div className={styles.container}>
+      <div className={styles.manage}>Manage</div>
+
+      <div className={styles.content}>CFO</div>
+    </div>
   )
 }
