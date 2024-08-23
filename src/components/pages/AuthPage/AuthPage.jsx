903500a0998af1153a672feb099ed1f8618eff33
Polina Kyzylova <kyzylovapa@mail.ru>
2024-08-23 22:04:55 +0500

    add: finish auth adaptive

--- src/components/pages/AuthPage/AuthPage.jsx
+++ src/components/pages/AuthPage/AuthPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import styles from './AuthPage.module.css';
 import { Outlet } from 'react-router-dom';
+import onboard from '../../../assets/onboard.svg';
 
 
 export default function AuthPage() {
     return (
         <div className={styles.container}>
             <div className={styles.onboard}>
-                onboard
+                <img src={onboard} alt='Onboard photo' />
             </div>
 
             <Outlet />
